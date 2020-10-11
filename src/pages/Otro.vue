ca2513e8b081af9aab6af2aa16095c5211e4c33a
Rommel Mendoza <rommelsmz@gmail.com>
2020-10-11 11:01:21 -0600

    Cambiando texto de nuevo

--- src/pages/Otro.vue
+++ src/pages/Otro.vue
@@ -2,7 +2,7 @@
   <div class="flex" style="height: 400px">
     <div class="items-center self-center">
       <p class="text-h4 text-indigo text-center">
-        Esta es otra página
+        Esta es otra página para probar el router
         </p>
     </div>
   </div>
